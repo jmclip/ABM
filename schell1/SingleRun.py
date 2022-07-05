61d8df8aeb88edd4e02bf548f16691c68aaad48c
jmclip <41075491+jmclip@users.noreply.github.com>
2022-07-05 12:27:37 -0500

    Delete SingleRun.py
    redundant

--- schell1/SingleRun.py
+++
@@ -1,8 +0,0 @@
-from model import SegModel
-from mesa.datacollection import DataCollector
-
-model = SegModel(20, 30, 0.8, 0.2, 0.375, 0.375)
-for t in range(10):
-    model.step()
-
-model_df = model.datacollector.get_model_vars_dataframe()
